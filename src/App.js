45bd863d73fee04cba10b2727602e1cbcf563779
Robert Snow <robjsnow@gmail.com>
2023-01-16 03:20:00 -0600

    completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bulma/css/bulma.css' //go into the node modules and find bulma
+import ProfileCard from './ProfileCard'
+import AlexaImage from './images/alexa.png';
+import CortanaImage from './images/cortana.png';
+import SiriImage from './images/siri.png';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+        <div>
+            <div>
+                <section className="hero is-primary">
+                    <div className="hero-body">
+                        <p className="title"> Personal Digital Assistance</p>
+                    </div>
+                </section>
 
+            </div>
+            <div className="container">
+                <section className="section">
+                    <div className="columns">
+                       <div className="column is-3">
+                       <ProfileCard title= "Alexa"
+                       handle= "@alexa99"
+                       image={AlexaImage}
+                       description="Alexa was created by Amazon and helps you buy things."/>
+                       </div>
+                       <div className="column is-3">
+                       <ProfileCard title = "Cortana"
+                       handle= "@cortana32"
+                       image={CortanaImage}
+                       description="Made by Microsoft who knows what it does."
+                       />
+                        </div>
+                        <div className="column is-3">
+                        <ProfileCard
+                        title = "Siri"
+                        handle= "@siri01"
+                        image={SiriImage}
+                        description="Siri was made by apple and is being phased out."
+                        />
+                        </div>
+                    </div>
+                </section>
+            </div>
+       </div>
+    );
+}
 export default App;
